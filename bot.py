044e358b30c4ab06647c9faa72ade29b3d43d145
fernando <rluis4490@gmail.com>
2020-02-22 12:49:27 -0600

    fixed main.py errors

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ class Bot(object):
     def handle(self,user_id,user_message):
         logging.info("Se invoco el metodo handle")
         #get history events and messages
-        message_text = tree['say']
-        possible_answers = tree['answers'].keys()
+        message_text = self.tree['say']
+        possible_answers = self.tree['answers'].keys()
+        possible_answers.sort()
         self.send_callback(user_id,message_text,possible_answers)
